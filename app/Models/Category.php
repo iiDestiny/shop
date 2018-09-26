9a1dc65550ef1f1febfe706784c569e4ce9377d3
罗彦 <iidestiny@vip.qq.com>
2018-09-26 15:07:41 +0800

    feat(删除 w):

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -94,7 +94,7 @@ class Category extends Model
      * 定义一个访问器，获取以 - 为分隔的所有祖先类目名称以及当前类目的名称
      *
      * @return mixed
-     */w
+     */
     public function getFullNameAttribute()
     {
         return $this->ancestors// 获取所有祖先类目
