4b9097d1787670d6f3c03f88b7c037fceeedb306
罗彦 <iidestiny@vip.qq.com>
2018-10-08 15:13:04 +0800

    feat(支付宝分期付款):

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
         Route::get('installments', 'InstallmentsController@index')->name('installments.index');
         Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
+        Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
+        Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
     });
     // 结束
 });
@@ -57,3 +59,5 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
+// 分期付款回调
+Route::post('installments/alipay/notify', 'InstallmentsController@alipayNotify')->name('installments.alipay.notify');
